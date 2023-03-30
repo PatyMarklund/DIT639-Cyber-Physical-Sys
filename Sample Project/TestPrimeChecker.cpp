98c41ac5faca0629b0281bf53f74263d3c6543ef
Harring <harring_@hotmail.com>
2023-03-30 09:55:15 +0200

    Fix #2 add sample project

---
+++ Sample Project/TestPrimeChecker.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "PrimeChecker.hpp"
+
+TEST_CASE("Test PrimeChecker 1.") {
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(5));
+}
+
+TEST_CASE("Test PrimeChecker 2.") {
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(7));
+}
+
+TEST_CASE("Test PrimeChecker 3.") {
+    PrimeChecker pc;
+    REQUIRE(!pc.isPrime(12));
+}
